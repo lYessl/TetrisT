f7a16eed13360836df7293a97901ad8ea11c37d5
Yess <woooku@gmail.com>
2021-03-25 12:46:28 +0300

    Mistake. When figure move down points create.

--- Tetris/Figure.cs
+++ Tetris/Figure.cs
@@ -6,7 +6,8 @@ namespace Tetris
 {
     abstract class Figure
     {
-        protected Point[] points = new Point[4];
+        const int LENGHT = 4;
+        protected Point[] points = new Point[LENGHT];
 
         public void Draw()
         {
@@ -15,14 +16,55 @@ namespace Tetris
                 p.Draw();
             }
         }
-
-        public void Move(Direction dir)
+        internal void TryMove(Direction dir)
         {
-            foreach(Point p in points)
+            Hide();
+            var clone = Clone();
+            Move(clone, dir);
+            if (VerifyPosition(clone))
+                points = clone;
+
+
+            Draw();
+
+        }
+
+        private bool VerifyPosition(Point[] pList)
+        {
+            foreach(var p in pList)
+            {
+                if (p.x < 0 || p.y < 0 || p.x >= 40 || p.y >= 30)
+                    return false;
+            }
+            return true;
+        }
+
+        private Point[] Clone()
+        {
+            var newPoints = new Point[LENGHT];
+            for (int i = 0; i < LENGHT; i++)
+            {
+                newPoints[i] = new Point(points[i]);
+            }
+            return newPoints;
+        }
+        public void Move(Point[] pList, Direction dir)
+        {
+            foreach(var p in pList)
             {
                 p.Move(dir);
             }
         }
+
+        //public void Move(Direction dir)
+        //{
+        //    Hide();
+        //    foreach(Point p in points)
+        //    {
+        //        p.Move(dir);
+        //    }
+        //    Draw();
+        //}
         public void Hide()
         {
             foreach (Point p in points)
@@ -32,5 +74,6 @@ namespace Tetris
         }
 
         public abstract void Rotate();
+
     }
 }
--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -19,48 +19,94 @@ namespace Tetris
             //    fig.Draw();
             //}
 
-            Figure s = new Stick(20, 5, '*');
-            s.Draw();
+            FigureGenerator generator = new FigureGenerator(20, 0, '*');
+            Figure currentFigure = generator.GetNewFigure();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Rotate();
-            s.Draw();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    HandleKey(currentFigure, key);
+                }
+                //FigureFall(out s, generator);
+                //s.Draw();
+            }
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Rotate();
-            s.Draw();
+            //s.Draw();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.LEFT);
-            s.Draw();
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Rotate();
+            //s.Draw();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.DOWN);
-            s.Draw();
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Rotate();
+            //s.Draw();
 
-            Thread.Sleep(500);
-            s.Hide();
-            s.Move(Direction.RIGHT);
-            s.Draw();
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Move(Direction.LEFT);
+            //s.Draw();
+
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Move(Direction.DOWN);
+            //s.Draw();
+
+            //Thread.Sleep(500);
+            //s.Hide();
+            //s.Move(Direction.RIGHT);
+            //s.Draw();
             //Stick stick = new Stick(5, 8, '*');
             //stick.Draw();
 
-            Point p1 = new Point(2, 3, '*');
-            p1.Draw();
+            //    Point p1 = new Point(2, 3, '*');
+            //    p1.Draw();
 
-            Point p2 = new Point()
-            {
-                x = 4,
-                y = 5,
-                c = '#'
-            };
-            p2.Draw();
+            //    Point p2 = new Point()
+            //    {
+            //        x = 4,
+            //        y = 5,
+            //        c = '#'
+            //    };
+            //    p2.Draw();
 
-            Console.ReadLine();
+            //    Console.ReadLine();
+            //}
+
+            //static void FigureFall(out Figure fig, FigureGenerator generator)
+            //{
+            //    fig = generator.GetNewFigure();
+            //    fig.Draw();
+
+            //    for (int i = 0; i < 15; i++)
+            //    {
+            //        fig.Hide();
+            //        fig.Move(Direction.DOWN);
+            //        fig.Draw();
+            //        Thread.Sleep(200);
+            //    }
+
         }
+
+            private static void HandleKey(Figure currentFigure, ConsoleKeyInfo key)
+            {
+            switch (key.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    currentFigure.TryMove(Direction.LEFT);
+                    break;
+
+                case ConsoleKey.RightArrow:
+                    currentFigure.TryMove(Direction.RIGHT);
+                    break;
+                case ConsoleKey.DownArrow:
+                    currentFigure.TryMove(Direction.DOWN);
+                    break;
+            }
+        }
+        
     }
 }
